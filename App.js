9f3d319cebad9a409dc94f9ff20850913573e622
Saurabh Maurya <iamheremaurya@gmail.com>
2021-10-14 00:54:16 +0530

    first commit
    added restaurant info screen

---
+++ App.js
@@ -0,0 +1,16 @@
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet } from "react-native";
+import { RestaurantsScreen } from "./src/features/restaurants/screens/RestaurantsScreen";
+
+
+export default function App() {
+  return (
+    <>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
+    </>
+  );
+}
+
+const styles = StyleSheet.create({});
